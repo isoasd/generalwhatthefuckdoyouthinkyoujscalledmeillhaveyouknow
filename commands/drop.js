8f66cc475f9247c891e6cb0aff043c22168ef20a
haevanhoe quihuata <haevanhoe@gmail.com>
2018-10-07 12:40:40 -0300

    esp

--- commands/drop.js
+++ commands/drop.js
@@ -2,28 +2,29 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, config) => {
     const landings = [
-        "Wailing Woods", "Shifty Shafts",
-        "Tomato Temple", "Greasy Grove",
-        "Salty Springs", "Retail Row",
-        "Dusty Divot", "Tilted Towers",
-        "Junk Junction", "Haunted Hills",
-        "Snobby Shores", "Lazy Links",
-        "Risky Reels", "Lonely Lodge",
-        "Paradise Palms", "Loot Lake",
-        "Fatal Fields", "Flush Factory",
-        "Pleasant Park "];
+        "Alameda Aullante", "Tuneles Tortuosos",
+        "Templo Tomate", "Caserio Colesterol",
+        "Señorio de la Sal", "Ciudad Comercio",
+        "Socavon Soterrado", "Pisos Picados",
+        "Chiringuito Chatarra", "Lomas Lugubres",
+        "Ribera Repipi", "Sociedad Sibarita",
+        "Carretes Comprometidos", "Soto Solitario",
+        "Oasis Ostentoso", "Aterrizaje Afortunado",
+        "Latifundio Letal", "Industrias Inodoras",
+        "Parque Placentero", "Pueblo Vikingo",
+        "Ex Balsa Botin", "Isla Flotante"];
     let drop = landings[parseInt(Math.random() * landings.length)]
 
     const attachment = new Discord.Attachment(`./locations/${drop}.png`);
     let embed = new Discord.RichEmbed()
-        .setAuthor(`Hey where am I dropping?`, message.guild.iconURL)
-        .setDescription(`You should drop at ** ${drop}**`)
+        .setAuthor(`Hey donde estoy cayendo?`, message.guild.iconURL)
+        .setDescription(`Deberías caer en ** ${drop}**`)
         //  .setDescription(`You should drop at ** ${drop}**.\n\n\nChest locations for ${drop} are shown below.`)
-        .setFooter(`Requested by ${message.author.username}`)
+        .setFooter(`Solicitado por ${message.author.username}`)
         .setTimestamp()
         .setColor(0x62c1f0)
     message.channel.send(embed);
-    message.channel.send(attachment);
+    //message.channel.send(attachment);
 }
 
 module.exports.help = {
